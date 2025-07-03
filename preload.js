a24a63397c9a77e6a683fa6cfe658449e59e6f7c
Yarlin Lynn <yarlinlynn@Yarlins-MacBook-Air.local>
2025-07-03 14:58:36 +0200

    Project Changes
    create folder structure, delete older project files and write up html for main project page, adding styles.css

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  loadPage: (page) => ipcRenderer.send("load-page", page),
-  takeScreenshot: () => ipcRenderer.invoke("take-screenshot"),
-});
