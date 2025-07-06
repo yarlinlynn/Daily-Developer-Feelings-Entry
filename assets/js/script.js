2bc8300da74505e570269932009e9c484f5738d9
Yarlin Lynn <yarlinlynn@Yarlins-MacBook-Air.local>
2025-07-06 12:30:22 +0200

    Add script.js to html
    link js file to html, creating the hover states for my emotoion buttons changing my background when hovering over my button

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+// DECLARE EMOTION BUTTONS IN NEW-ENTRY.HTML
+
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
+const btn6 = document.getElementById('btn6')
+const btn7 = document.getElementById('btn7')
+const btn8 = document.getElementById('btn8')
+
+// HOEVER STATES FOR EMOTIONS BUTTONS
+
+btn2.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#ffb3c8';
+})
+btn2.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = 'acc794';
+})
+
+btn3.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#75e4ff';
+})
+btn3.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = '#acc794';
+})
+
+btn4.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#ffce3f';
+})
+btn4.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = '#acc794';
+})
+
+btn5.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#83da79';
+})
+btn5.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = '#acc794';
+})
+
+btn6.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#e88015';
+})
+btn6.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = '#acc794';
+})
+
+btn7.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#7897d4';
+})
+btn7.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = '#acc794';
+})
+
+btn8.addEventListener("mouseover", function() {
+  document.querySelector('body').style.backgroundColor = '#a4b4d4';
+})
+btn8.addEventListener("mouseleave", function() {
+  document.querySelector('body').style.backgroundColor = '#acc794';
+})
